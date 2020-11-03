a775ea262fc5ca949ffda68703c74fef3db93cba
Justin Ballerstein <justin.ballerstein@va.gov>
2020-11-03 12:36:35 -0500

    Added the comment to show unit test structure

--- unit.py
+++ unit.py
@@ -4,10 +4,13 @@ import pythagorean
 class TestStringMethods(unittest.TestCase):
 
     def test_three_four_five_triangle(self):
+        # arrange
         a = 3
         b = 4
         expected_c = 5
+        # act
         c = pythagorean.Pythagorean.theorem(a,b)
+        # assert
         self.assertEqual(c, expected_c)
 
 if __name__ == '__main__':
